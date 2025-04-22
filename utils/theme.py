41b3131ecb4f9de4c8e9a94da82c3ed6fab4edba
itsmearion <bilqiss0893@gmail.com>
2025-04-22 15:59:35 +0700

    Create theme.py

---
+++ utils/theme.py
@@ -0,0 +1,7 @@
+# utils/theme.py
+
+WELCOME_MESSAGES = [
+    "༄❀ Delicate petals drift around you... ᯓ༄",
+    "༄ Feathers of dreams flutter in the twilight ~ ❀༄",
+    "𖤓 Pick your charm, dear traveller ~"
+]
